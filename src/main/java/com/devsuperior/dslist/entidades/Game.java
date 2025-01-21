5bf9ee894129bd9d4db35fff04dafc15da70a6b4
Vitor <vitinhoc2005@gmail.com>
2025-01-21 14:16:57 -0300

    properties, Game, ORM

---
+++ src/main/java/com/devsuperior/dslist/entidades/Game.java
@@ -0,0 +1,119 @@
+package com.devsuperior.dslist.entidades;
+
+import java.util.Objects;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String titulo;
+
+    @Column(name = "game_year")
+    private int ano;
+    private String genero;
+    private String plataforma;
+    private String imgUrl;
+    private String descricaoCurta;
+    private String descricaoLonga;
+
+    public Game(String descricaoLonga, String descricaoCurta, String imgUrl, String plataforma, String genero, int ano, String titulo, long id) {
+        this.descricaoLonga = descricaoLonga;
+        this.descricaoCurta = descricaoCurta;
+        this.imgUrl = imgUrl;
+        this.plataforma = plataforma;
+        this.genero = genero;
+        this.ano = ano;
+        this.titulo = titulo;
+        this.id = id;
+    }
+    public Game() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getPlataforma() {
+        return plataforma;
+    }
+
+    public void setPlataforma(String plataforma) {
+        this.plataforma = plataforma;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getDescricaoCurta() {
+        return descricaoCurta;
+    }
+
+    public void setDescricaoCurta(String descricaoCurta) {
+        this.descricaoCurta = descricaoCurta;
+    }
+
+    public String getDescricaoLonga() {
+        return descricaoLonga;
+    }
+
+    public void setDescricaoLonga(String descricaoLonga) {
+        this.descricaoLonga = descricaoLonga;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return id == game.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
